53f7524bde4d9d9dd76bea285b322fe168611af4
DimaSamborski <SamborskiDima@ukr.net>
2016-03-24 23:32:56 +0200

    First step
    create class for work in db

---
+++ AtP_Curs/AtP_Curs/App_data/DBWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtP_Curs.App_data
+{
+    // Class for work on db
+    class DBWork
+    {
+        // method check users in table
+        public bool Check()
+        {
+            // to do: check people in database;
+
+            return false;
+        }
+    }
+}
--- AtP_Curs/AtP_Curs/Form1.cs
+++ AtP_Curs/AtP_Curs/Form1.cs
@@ -16,5 +16,7 @@ namespace AtP_Curs
         {
             InitializeComponent();
         }
+
+        
     }
 }
